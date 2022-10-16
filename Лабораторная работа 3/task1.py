5deb35ac14f5673b6dd0e709328b4c0022e46661
daniel-ivn <115413617+daniel-ivn@users.noreply.github.com>
2022-10-16 21:44:18 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task1.py
@@ -0,0 +1,10 @@
+src = not False and True or False and not True
+
+# TODO расписать упрощение выражения
+scr_next_1 = True and True or False and False
+scr_next_2 = True or False
+sqr_result = True
+
+result = sqr_result  # TODO подставить результат выражения
+
+print(src == result)
